ff2da448983a3bbad208cc4cca05fa1c52934f43
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-01-28 21:58:52 +0800

    List for latex

---
+++ lisp/personal/private/init-latex.el
@@ -0,0 +1,12 @@
+;;LaTeX config
+(add-hook 'LaTeX-mode-hook
+        (lambda ()
+          (setq TeX-auto-untabify t     ; remove all tabs before saving
+                TeX-engine 'xetex       ; use xelatex default
+                TeX-show-compilation t) ; display compilation windows
+          (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
+          (setq TeX-save-query nil)
+          (imenu-add-menubar-index)
+          (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)))
+
+(provide 'init-latex)
