00a1b6de2109574ed36ce365bdc979946550582d
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-03-10 23:18:28 +0800

    Add racket mode

--- lisp/local/others/init-racket-mode.el
+++ lisp/local/others/init-racket-mode.el
@@ -2,5 +2,12 @@
 
 (setq tab-always-indent 'complete)
 
+(add-auto-mode 'racket-mode "\\.rkt\\'")
+
+(setq auto-mode-alist
+  (append auto-mode-alist
+          '(("\\.rkt\\'" . racket-mode))
+   ))
+
 (provide 'init-racket-mode)
 ;;; init-racket-mode.el ends here
