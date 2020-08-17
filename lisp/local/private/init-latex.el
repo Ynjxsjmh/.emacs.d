ec0a233126f4a6e2957695eb14a76173afc15fc7
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-18 00:26:01 +0800

    Edit latex configuration

--- lisp/local/private/init-latex.el
+++ lisp/local/private/init-latex.el
@@ -1,12 +1,18 @@
 ;;LaTeX config
 (add-hook 'LaTeX-mode-hook
         (lambda ()
+          (add-to-list 'tex-compile-commands '("xelatex --shell-escape -interaction nonstopmode -output-directory %o %f" t "%r.pdf"))
           (setq TeX-auto-untabify t     ; remove all tabs before saving
+                TeX-show-compilation t  ; display compilation windows
+                TeX-save-query nil
                 TeX-engine 'xetex       ; use xelatex default
-                TeX-show-compilation t) ; display compilation windows
+                ;; TeX-command-default "XeLaTeX"
+                )
           (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
-          (setq TeX-save-query nil)
           (imenu-add-menubar-index)
           (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)))
+
+
+(add-hook 'LaTeX-mode-hook #'latex-extra-mode)
 
 (provide 'init-latex)
