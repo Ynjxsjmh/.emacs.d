d872e9dbcdd969e84058f3250ac0a5204dbc1879
Ynjxsjmh <ynjxsjmh@gmail.com>
2021-07-04 18:35:11 +0800

    Insert real whitespace mode in org and asciidoc mode
    markdown is used often, don't edit original format

--- lisp/local/others/init-pangu-spacing.el
+++ lisp/local/others/init-pangu-spacing.el
@@ -2,9 +2,11 @@
 
 (global-pangu-spacing-mode 1)
 
-(add-hook 'markdown-mode-hook
+;; only insert real whitespace in some specific mode, but just add virtual space in other mode
+
+(add-hook 'org-mode-hook
           '(lambda ()
-           (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)))
+             (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)))
 
 (add-hook 'adoc-mode-hook
           '(lambda ()
