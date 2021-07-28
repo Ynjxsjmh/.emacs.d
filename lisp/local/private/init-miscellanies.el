554f48d651d44f4c9e3f9b52036e942b5ee4eda8
Ynjxsjmh <ynjxsjmh@gmail.com>
2021-07-28 14:53:23 +0800

    Implement function to convert string to unicode

--- lisp/local/private/init-miscellanies.el
+++ lisp/local/private/init-miscellanies.el
@@ -128,6 +128,41 @@
         (insert " ")
         (forward-line 1)))))
 
+(setq superscript-subscript-map
+      ;;     sup sub
+      '(("a" "ᵃ" "ₐ") ("b" "ᵇ" nil) ("c" "ᶜ" nil) ("d" "ᵈ" nil)
+        ("e" "ᵉ" "ₑ") ("f" "ᶠ" nil) ("g" "ᵍ" nil) ("h" "ʰ" "ₕ")
+        ("i" "ⁱ" "ᵢ") ("j" "ʲ" "ⱼ") ("k" "ᵏ" "ₖ") ("l" "ˡ" "ₗ")
+        ("m" "ᵐ" "ₘ") ("n" "ⁿ" "ₙ") ("o" "ᵒ" "ₒ") ("p" "ᵖ" "ₚ")
+        ("q" nil nil) ("r" "ʳ" "ᵣ") ("s" "ˢ" "ₛ") ("t" "ᵗ" "ₜ")
+        ("u" "ᵘ" "ᵤ") ("v" "ᵛ" "ᵥ") ("w" "ʷ" nil) ("x" "ˣ" "ₓ")
+        ("y" "ʸ" nil) ("z" "ᶻ" nil)
+        ("A" "ᴬ" nil) ("B" "ᴮ" nil) ("C" nil nil) ("D" "ᴰ" nil)
+        ("E" "ᴱ" nil) ("F" nil nil) ("G" "ᴳ" nil) ("H" "ᴴ" nil)
+        ("I" "ᴵ" nil) ("J" "ᴶ" nil) ("K" "ᴷ" nil) ("L" "ᴸ" nil)
+        ("M" "ᴹ" nil) ("N" "ᴺ" nil) ("O" "ᴼ" nil) ("P" "ᴾ" nil)
+        ("Q" nil nil) ("R" "ᴿ" nil) ("S" nil nil) ("T" "ᵀ" nil)
+        ("U" "ᵁ" nil) ("V" "ⱽ" nil) ("W" "ᵂ" nil) ("X" nil nil)
+        ("Y" " " nil) ("Z" " " nil)
+        ("0" "⁰" "₀") ("1" "¹" "₁") ("2" "²" "₂") ("3" "³" "₃")
+        ("4" "⁴" "₄") ("5" "⁵" "₅") ("6" "⁶" "₆") ("7" "⁷" "₇")
+        ("8" "⁸" "₈") ("9" "⁹" "₉")
+        ("+" "⁺" "₊") ("-" "⁻" "₋") ("=" "⁼" "₌") ("(" "⁽" "₍")
+        (")" "⁾" "₎")
+        ("ɑ" "ᵅ" nil) ("β" "ᵝ" "ᵦ") ("θ" "ᶿ" nil)))
+
+(defun convert-to-unicode (str subsup)
+  (let ((converted-str ""))
+    (progn
+      (dolist (c (delete "" (split-string str "")))
+        (cond ((string-equal subsup "^")
+               (setq converted-str (concat converted-str (nth 0 (cdr (assoc c superscript-subscript-map))))))
+              ((string-equal subsup "_")
+               (setq converted-str (concat converted-str (nth 1 (cdr (assoc c superscript-subscript-map))))))
+              (t "default")))
+      (if (eq (length str) (length converted-str))
+          converted-str
+        str))))
 ;;------------------------------------------------------------------------------
 
 ;; https://wikemacs.org/wiki/Emacs_Lisp_Cookbook
